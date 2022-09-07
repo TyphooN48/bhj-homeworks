35095df4f5bd15c2255488456234dc87b8bb6716
Andrey Burlakov <43408988+TyphooN48@users.noreply.github.com>
2022-09-07 17:15:01 +0300

    update task

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,17 +1,17 @@
-const modal_main = document.getElementById('modal_main');
-modal_main.classList.add('modal_active');
+const modalMain = document.getElementById('modal_main');
+modalMain.classList.add('modal_active');
 
-const modal__close = document.getElementsByClassName('modal__close');
-const modal_success = document.getElementById('modal_success');
-for (let i = 0; i < modal__close.length; i++) {
-    modal__close.item(i).onclick = () => {
-        modal_main.classList.remove('modal_active');
-        modal_success.classList.remove('modal_active');
+const modalClose = document.getElementsByClassName('modal__close');
+const modalSuccess = document.getElementById('modal_success');
+for (let i = 0; i < modalClose.length; i++) {
+    modalClose.item(i).onclick = () => {
+        modalMain.classList.remove('modal_active');
+        modalSuccess.classList.remove('modal_active');
     };
 }
 
-const show_success = document.getElementsByClassName('show-success').item(0);
-show_success.onclick = () => {
-    modal_main.classList.remove('modal_active');
-    modal_success.classList.add('modal_active');
+const showSuccess = document.getElementsByClassName('show-success').item(0);
+showSuccess.onclick = () => {
+    modalMain.classList.remove('modal_active');
+    modalSuccess.classList.add('modal_active');
 };
