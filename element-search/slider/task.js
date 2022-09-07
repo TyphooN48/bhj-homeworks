d8a08fa6bdf0c487d796a77940a8764efd85e376
Andrey Burlakov <43408988+TyphooN48@users.noreply.github.com>
2022-09-07 17:14:21 +0300

    update task

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,49 +1,29 @@
-const slider__dot = document.getElementsByClassName('slider__dot');
-function slider(slide) {
-    const slider__item = document.getElementsByClassName('slider__item');
-    for (let i = 0; i < slider__item.length; i++) {
-        if (slider__item.item(i).classList.contains('slider__item_active')) {
-            slider__item.item(i).classList.remove('slider__item_active');
-        }
-        if (slider__dot.item(i).classList.contains('slider__dot_active')) {
-            slider__dot.item(i).classList.remove('slider__dot_active');
-        }
-    }
-    slider__item.item(slide).classList.add('slider__item_active');
-    slider__dot.item(slide).classList.add('slider__dot_active');
-}
+const sliderDot = Array.from(document.getElementsByClassName('slider__dot'));
+const sliderItems = Array.from(document.getElementsByClassName('slider__item'));
 
-for (let i = 0; i < slider__dot.length; i++) {
-    slider__dot.item(i).onclick = () => {
-        slider(i);
-    };
+sliderDot.forEach((el, ind) => el.addEventListener('click', () => slide(ind)));
+sliderDot[0].classList.add('slider__dot_active');
+
+
+function slide(slideInd) {
+    const currentActiveInd = sliderItems.findIndex(el => el.classList.contains('slider__item_active'));
+
+    sliderItems[currentActiveInd].classList.remove('slider__item_active');
+    sliderDot[currentActiveInd].classList.remove('slider__dot_active');
+
+    sliderItems[slideInd].classList.add('slider__item_active');
+    sliderDot[slideInd].classList.add('slider__dot_active');
+
 }
 
 document.getElementsByClassName('slider__arrow_next').item(0).onclick = () => {
-    const slider__item = document.getElementsByClassName('slider__item');
-    for (let i = 0; i < slider__item.length; i++)
-    {
-        if (slider__item.item(i).classList.contains('slider__item_active')) {
-            if (i + 1 == slider__item.length) {
-                slider(0);
-                break;
-            }
-            slider(i + 1);
-            break;
-        }
-    }
+    let currentInd = sliderItems.findIndex((el) => el.classList.contains('slider__item_active')) + 1;
+    slide(currentInd === sliderItems.length ? 0 : currentInd);
+
 };
 
 document.getElementsByClassName('slider__arrow_prev').item(0).onclick = () => {
-    const slider__item = document.getElementsByClassName('slider__item');
-    for (let i = 0; i < slider__item.length; i++) {
-        if (slider__item.item(i).classList.contains('slider__item_active')) {
-            if (i - 1 == -1) {
-                slider(slider__item.length - 1);
-                break;
-            }
-            slider(i - 1);
-            break;
-        }
-    }
-}
+    let currentInd = sliderItems.findIndex((el) => el.classList.contains('slider__item_active')) - 1;
+    slide(currentInd < 0 ? sliderItems.length - 1 : currentInd);
+
+};
