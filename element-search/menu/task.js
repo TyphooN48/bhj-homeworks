e61b90885e030205c37f35e1c3bd153b22bddb53
Andrey Burlakov <43408988+TyphooN48@users.noreply.github.com>
2022-09-07 17:15:29 +0300

    update task

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,12 +1,14 @@
-const menu__link = document.getElementsByClassName('menu__link');
-const menu__item = document.getElementsByClassName('menu__item');
+const menuLink = document.getElementsByClassName('menu__link');
+const menuItem = document.getElementsByClassName('menu__item');
 
-for (let i = 0; i < menu__link.length; i++) {
-    menu__link.item(i).onclick = () => {
+for (let i = 0; i < menuLink.length; i++) {
+    menuLink.item(i).onclick = () => {
         for (let j = 0; j < document.querySelectorAll('ul.menu_active').length; j++) {
             document.querySelectorAll('ul.menu_active').item(j).classList.remove('menu_active');
+            return false;
         }
-        menu__item.item(i).querySelector('ul').classList.toggle('menu_active');
+        if(menuItem.item(i).querySelector('ul'))
+            menuItem.item(i).querySelector('ul').classList.toggle('menu_active');
         return false;
     };
 }
